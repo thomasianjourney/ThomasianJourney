4d4639c825e5a5fc1a84174e8fe45bfae0f5024c
charmagneadonis <charmagnechandrah.adonis.iics@ust.edu.ph>
2019-11-27 00:58:32 +0800

    Initial Commit

---
+++ ThomasianJourney/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThomasianJourney
+//
+//  Created by Charmagne Adonis on 11/27/19.
+//  Copyright © 2019 Capstone Project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
