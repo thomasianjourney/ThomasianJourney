0852dc330d3e5c7169c2386c0317a2b780c32b83
charmagneadonis <thomasianjourney@ust.edu.ph>
2020-03-31 13:42:28 +0800

    Event Dates Added, Fixed Formatting For EventDetails

--- ThomasianJourney/EventCell.swift
+++ ThomasianJourney/EventCell.swift
@@ -10,16 +10,71 @@ import UIKit
 
 class EventCell: UITableViewCell {
 
-    @IBOutlet var date: UIButton!
+    @IBOutlet var dateBox: UILabel!
     @IBOutlet var eventTitle: UILabel!
     @IBOutlet var eventDescription: UILabel!
     @IBOutlet var eventStatus: UILabel!
     
     func setTitle (event: AllEventDetails) {
+        
+        let status = event.status
+        let date = event.eventDate.components(separatedBy: CharacterSet(charactersIn: "-: "))
+        var month = date[1]
+        let year = date[0]
+        let day = date[2]
+        print ("DATE TESTING FOR FORMATTING \(date)")
+        
+        switch status {
+            
+        case "absent":
+            eventStatus.text = "Not Attended"
+        case "upcoming":
+            eventStatus.text = "Event Not Yet Available"
+            eventStatus.textColor = UIColor(red: 255, green: 140, blue: 0, alpha: 1)
+        case "available":
+            eventStatus.text = "Event Available"
+            eventStatus.textColor = UIColor(red: 0, green: 128, blue: 0, alpha: 1)
+        case "cancelled":
+            eventStatus.text = "Cancelled"
+            eventStatus.textColor = UIColor(red: 178, green: 34, blue: 34, alpha: 1)
+        default:
+            eventStatus.text = "Attended"
+            eventStatus.textColor = UIColor(red: 0, green: 128, blue: 0, alpha: 1)
+        }
+        
+        switch month {
+        case "01":
+            month = "Jan"
+        case "02":
+            month = "Feb"
+            break;
+        case "03":
+            month = "Mar"
+        case "04":
+            month = "April"
+        case "05":
+            month = "May"
+        case "06":
+            month = "Jun"
+        case "07":
+            month = "Jul"
+        case "08":
+            month = "Aug"
+        case "09":
+            month = "Sep"
+        case "10":
+            month = "Oct"
+        case "11":
+            month = "Nov"
+        case "12":
+            month = "Dec"
+        default:
+            month = ""
+        }
+        
+        dateBox.text = "\(month) \(day) \(year)"
         eventTitle.text = event.activityName
         eventDescription.text = event.eventVenue
-        eventStatus.text = event.status
-        //print (event.activityName)
     }
 
 }
