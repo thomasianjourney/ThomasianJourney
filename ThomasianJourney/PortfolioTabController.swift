5d1ccb495c56ce24489c8cb58fdb6dbc0edbebc3
charmagneadonis <thomasianjourney@ust.edu.ph>
2020-05-05 01:19:56 +0800

    Events and Portfolio View Help added

---
+++ ThomasianJourney/PortfolioTabController.swift
@@ -0,0 +1,27 @@
+//
+//  PortfolioTabController.swift
+//  ThomasianJourney
+//
+//  Created by Charmagne Adonis on 5/5/20.
+//  Copyright © 2020 Capstone Project. All rights reserved.
+//
+
+import UIKit
+
+class PortfolioTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "icon_help_white_small"), style: .plain, target: self, action: #selector(goToViewHelp))
+    }
+    
+    @objc func goToViewHelp() {
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let showViewHelp =
+        main.instantiateViewController(withIdentifier: Constants.Storyboard.viewHelp) as! ViewHelpController
+        
+        present(showViewHelp, animated: true, completion: nil)
+    }
+
+}
